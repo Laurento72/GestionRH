0a257b0bdf2c3e804a646454b5dd15f45fb8a874
Laurento72 <145332721+Laurento72@users.noreply.github.com>
2023-09-18 11:29:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Info;
+
+namespace GestionRH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe e = new Employe("Martin", "Jean", "Paris", 35, "Technicien", 2500.0);
+            Console.WriteLine(e.getInfo());
+            e.affectation("Technicien Senior");
+            e.augmentation(500);
+            Console.WriteLine(e.getInfo());
+
+
+            using (Employe ee = new Employe("O", "L", "F", 50, "Atos", 5000.0))
+            {
+                Console.WriteLine(ee.getInfo());
+            }
+
+        }
+    }
+}
